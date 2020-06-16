9147fe0dccf3cb7985fe98e2732c9b505ade0c2e
Cristian Rojas <injcristianrojas@gmail.com>
2020-06-16 18:32:08 -0400

    New LCD library

--- lcd_control.py
+++ lcd_control.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from lcd.lcddriver import Lcd
+from RPi_I2C_driver import Lcd
 
 from datetime import datetime
 
@@ -9,8 +9,8 @@ def get_strings(internal_temp, external_temp):
     now_utc = datetime.utcnow().strftime('%H:%M')
     return [
         'SCL {} UTC {}'.format(now_cl, now_utc)[:20],
-        'Inside temp: {0:0.1f}°C'.format(internal_temp)[:20],
-        'Outside temp: {0:0.1f}°C'.format(external_temp)[:20],
+        'Inside temp: {0:0.1f}{}C'.format(internal_temp,chr(176))[:20],
+        'Outside temp: {0:0.1f}{}°C'.format(external_temp,chr(176))[:20],
     ]
 
 def show_data_mock(internal_temp, external_temp):
@@ -20,6 +20,7 @@ def show_data_mock(internal_temp, external_temp):
 
 def show_data(internal_temp, external_temp):
     lcd = Lcd()
+    lcd.backlight(0)
     strings = get_strings(internal_temp, external_temp)
     for i in range(0, len(strings[:4])):
         lcd.lcd_display_string(strings[i], i+1)
