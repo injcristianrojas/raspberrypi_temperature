bf328fa3322f462e6ae65d67eadb37f3ca3f5707
Cristian Rojas <injcristianrojas@gmail.com>
2020-06-18 12:09:48 -0400

    Comments on better display

--- lcd_control.py
+++ lcd_control.py
@@ -23,3 +23,16 @@ def show_data(internal_temp, external_temp):
     strings = get_strings(internal_temp, external_temp)
     for i in range(0, len(strings[:4])):
         lcd.lcd_display_string(strings[i], i+1)
+
+def set_individual_data(internal_temp, external_temp):
+    # BEWARE: Convert this to Object-Oriented, because Lcd() resets the display.
+    # Times
+    # lcd.lcd_display_string_pos('21:25', 1, 4)
+    # lcd.lcd_display_string_pos('21:25', 1, 14)
+    # Temps
+    # lcd.lcd_display_string_pos('27.1', 2, 13)
+    # lcd.lcd_display_string_pos('27.1', 3, 14)
+    # lcd.lcd_write(0xD1)
+    # lcd.lcd_write(0xA6)
+    # lcd.lcd_write_char(0xDF)
+    pass
