f3e2c26e2f402604c265744596ede5a2d557b080
Cristian Rojas <injcristianrojas@gmail.com>
2020-08-11 18:21:08 -0400

    Moved LOCATION_ID to .env

--- owmapi.py
+++ owmapi.py
@@ -5,15 +5,14 @@ import requests
 from flask import jsonify
 from dotenv import load_dotenv
 
-LOCATION_ID = 3875139 # Providencia, CL
-
 def dekelvinize(temp):
     return temp - 273.15
 
 def get_owmapi_data():
     load_dotenv()
     OWMAPI_KEY = os.getenv('OWMAPI_KEY')
-    url = "https://api.openweathermap.org/data/2.5/weather?id=%s&appid=%s" % (LOCATION_ID, OWMAPI_KEY)
+    OWMAPI_LOCATION_ID = os.getenv('OWMAPI_LOCATION_ID')
+    url = "https://api.openweathermap.org/data/2.5/weather?id=%s&appid=%s" % (OWMAPI_LOCATION_ID, OWMAPI_KEY)
     r = requests.get(url)
     data = r.json()
     feels_like = dekelvinize(data['main']['feels_like'])
