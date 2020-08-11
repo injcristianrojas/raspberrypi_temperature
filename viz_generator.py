73c0d260e03e7508c7e2f6102bc4c1be2df02fc3
Cristian Rojas <injcristianrojas@gmail.com>
2020-08-11 16:58:06 -0400

    Better viz generation

--- viz_generator.py
+++ viz_generator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import time
+import sys
 
 import pandas as pd
 import seaborn as sns
@@ -12,25 +13,22 @@ from dateutil import tz
 
 from io import StringIO
 
-from db import get_last_24hours_csv
+from db_v2 import get_last_24hours_csv
 
 def generate_image():
     temps = pd.read_csv(StringIO(get_last_24hours_csv()))
     temps['time'] = temps['time'].apply(lambda t: datetime.strptime(t, '%Y-%m-%d %H:%M:%S.%f').replace(tzinfo=tz.tzutc()).astimezone(tz.tzlocal()))
-
-    back24hours = (datetime.now() - timedelta(days = 1)).replace(tzinfo=tz.gettz())
-    inside_last24hours = temps.loc[(temps['sensor'] == 0) & (temps['time'] > back24hours)]
-    outside_last24hours = temps.loc[(temps['sensor'] == 1) & (temps['time'] > back24hours)]
+    temps['time'] = pd.to_datetime(temps['time'])
+    temps = temps.set_index('time')
+    print(temps.dtypes)
 
     f, ax = plt.subplots(figsize=(8,5))
-    sns.lineplot(x='time', y='temperature', data=inside_last24hours, ax=ax)
-    sns.lineplot(x='time', y='temperature', data=outside_last24hours, ax=ax)
+    sns.lineplot(data=temps, ax=ax, dashes=False)
     ax.set_ylim(0,35)
     ax.tick_params(labelrotation=45)
     ax.set_title('Temperatures in the last 24 hours (Last updated: {})'.format(datetime.now().strftime('%H:%M')))
     ax.legend(labels=['Internal', 'External'])
-    dateformatter = DateFormatter('%H:%M')
-    dateformatter.set_tzinfo(tz.tzlocal())
+    dateformatter = DateFormatter('%H:%M', tz=tz.tzlocal())
     ax.xaxis.set_major_formatter(dateformatter)
 
     plt.tight_layout()
