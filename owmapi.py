fc0cd5eb01833147e25ecbec1214560d65dde188
Cristian Rojas <injcristianrojas@gmail.com>
2020-08-27 13:17:29 -0400

    More robust connections

--- owmapi.py
+++ owmapi.py
@@ -2,6 +2,7 @@
 
 import os
 import requests
+from urllib3.exceptions import ProtocolError
 from flask import jsonify
 from dotenv import load_dotenv
 
@@ -12,13 +13,17 @@ def get_owmapi_data():
     load_dotenv()
     OWMAPI_KEY = os.getenv('OWMAPI_KEY')
     OWMAPI_LOCATION_ID = os.getenv('OWMAPI_LOCATION_ID')
-    url = "https://api.openweathermap.org/data/2.5/weather?id=%s&appid=%s" % (OWMAPI_LOCATION_ID, OWMAPI_KEY)
-    r = requests.get(url)
-    data = r.json()
-    feels_like = dekelvinize(data['main']['feels_like'])
-    temp = dekelvinize(data['main']['temp'])
-    condition = data['weather'][0]['description']
-    return temp, feels_like, condition
+    try:
+        url = "https://api.openweathermap.org/data/2.5/weather?id=%s&appid=%s" % (OWMAPI_LOCATION_ID, OWMAPI_KEY)
+        r = requests.get(url)
+        data = r.json()
+        feels_like = dekelvinize(data['main']['feels_like'])
+        temp = dekelvinize(data['main']['temp'])
+        condition = data['weather'][0]['description']
+        return temp, feels_like, condition
+    except (ConnectionError, ProtocolError) as e:
+        print(str(e) + ' ' + str(e.__traceback__))
+        return None, None, 'OWMAPI error'
 
 if __name__ == "__main__":
     print(get_owmapi_data())
