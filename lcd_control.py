467ed9307443d7ad54128ee9cc6c174b433f316d
Cristian Rojas <injcristianrojas@gmail.com>
2020-08-27 15:41:29 -0400

    OWMAPI Error handling in display

--- lcd_control.py
+++ lcd_control.py
@@ -17,7 +17,10 @@ class LCDControl:
         now_utc = datetime.utcnow().strftime('%H:%M')
         self.lcd.lcd_display_string('SCL {}  UTC {}'.format(now_cl, now_utc)[:20], 1)
         self.lcd.lcd_display_string('In/out:  {}/{} C'.format(f'{internal_temp:4.1f}', f'{external_temp:4.1f}')[:20], 2)
-        self.lcd.lcd_display_string('T/Feel:  {}/{} C'.format(f'{owmapi_temp:4.1f}', f'{owmapi_feels_like:4.1f}')[:20], 3)
+        if owmapi_temp is None:
+            self.lcd.lcd_display_string('T/Feel:  {}/{} C'.format(f'{owmapi_temp:4.1f}', f'{owmapi_feels_like:4.1f}')[:20], 3)
+        else:
+            self.lcd.lcd_display_string('T/Feel:  {}/{} C'.format('----', '----')[:20], 3)
         self.lcd.lcd_display_string('W: {}'.format(condition.capitalize())[:20], 4)
         self.lcd.lcd_write(0xD2)
         self.lcd.lcd_write_char(0xDF)
@@ -33,6 +36,10 @@ class LCDControl:
     def set_current_data(self, internal_temp, external_temp, owmapi_temp, owmapi_feels_like, condition):
         self.lcd.lcd_display_string_pos(f'{internal_temp:4.1f}', 2, 9)
         self.lcd.lcd_display_string_pos(f'{external_temp:4.1f}', 2, 14)
-        self.lcd.lcd_display_string_pos(f'{owmapi_temp:4.1f}', 3, 9)
-        self.lcd.lcd_display_string_pos(f'{owmapi_feels_like:4.1f}', 3, 14)
+        if owmapi_temp is None:
+            self.lcd.lcd_display_string_pos(f'{owmapi_temp:4.1f}', 3, 9)
+            self.lcd.lcd_display_string_pos(f'{owmapi_feels_like:4.1f}', 3, 14)
+        else:
+            self.lcd.lcd_display_string_pos('----', 3, 9)
+            self.lcd.lcd_display_string_pos('----', 3, 14)
         self.lcd.lcd_display_string_pos(condition.capitalize()[:20].ljust(17), 4, 3)
