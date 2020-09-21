9dab509071e0f3530189f7d8931d5ad8a44d6e1d
Cristian Rojas <injcristianrojas@gmail.com>
2020-09-21 13:09:12 -0300

    Pidfile per-thread

--- background_processing.py
+++ background_processing.py
@@ -7,7 +7,7 @@ from lcd_control import LCDControl
 from viz_generator import generate_image
 from temp_sensor_handler import measure_insert_getdata
 from owmapi import get_owmapi_data
-from pid.decorator import pidfile
+from pid import PidFile
 
 # Times for operations in seconds
 TIME_BETWEEN_VIZ_GENERATION = 600
@@ -27,14 +27,15 @@ def measure():
             lcd.set_current_data(int_temp, ext_temp, owm_temp, owm_feels_like, condition)
         time.sleep(1)
 
-@pidfile()
 def main():
     thr_measurement = threading.Thread(target=measure, daemon=True)
     thr_visualization = threading.Thread(target=generate_visualization, daemon=True)
-    thr_measurement.start()
-    thr_visualization.start()
-    thr_measurement.join()
-    thr_visualization.join()
+    with PidFile(piddir='.'):
+        thr_measurement.start()
+        thr_measurement.join()
+    with PidFile(piddir='.'):
+        thr_visualization.start()
+        thr_visualization.join()
 
 if __name__ == "__main__":
     main()
