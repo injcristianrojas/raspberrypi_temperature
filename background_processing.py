c80979efff28df50cdb23b3b345e0fa56978cbb0
Cristian Rojas <injcristianrojas@gmail.com>
2020-09-21 13:01:06 -0300

    Pidfile functionality

--- background_processing.py
+++ background_processing.py
@@ -7,6 +7,7 @@ from lcd_control import LCDControl
 from viz_generator import generate_image
 from temp_sensor_handler import measure_insert_getdata
 from owmapi import get_owmapi_data
+from pid.decorator import pidfile
 
 # Times for operations in seconds
 TIME_BETWEEN_VIZ_GENERATION = 600
@@ -26,10 +27,14 @@ def measure():
             lcd.set_current_data(int_temp, ext_temp, owm_temp, owm_feels_like, condition)
         time.sleep(1)
 
-if __name__ == "__main__":
+@pidfile()
+def main():
     thr_measurement = threading.Thread(target=measure, daemon=True)
     thr_visualization = threading.Thread(target=generate_visualization, daemon=True)
     thr_measurement.start()
     thr_visualization.start()
     thr_measurement.join()
     thr_visualization.join()
+
+if __name__ == "__main__":
+    main()
